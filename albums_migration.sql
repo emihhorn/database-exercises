b34554aa486633a112070f91d0fa92969035095d
emihhorn <emilyhopehorn@gmail.com>
2021-03-29 16:49:12 -0500

    albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,9 +4,9 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
-    name VARCHAR(75) NOT NULL,
+    name VARCHAR(75) NOT NULL DEFAULT 'Untitled',
     release_date INT UNSIGNED,
     sales FLOAT UNSIGNED,
     genre VARCHAR(50),
     PRIMARY KEY (id)
-);
+    );
