ca0a1e7f08972d87aab0a649babf00bc4d6878ab
emihhorn <emilyhopehorn@gmail.com>
2021-03-30 15:10:11 -0500

    transaction

--- transactions_migration.sql
+++ transactions_migration.sql
@@ -2,10 +2,10 @@ USE bank_db;
 
 CREATE TABLE IF NOT EXISTS transactions(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    transaction_date new DATE,
+    transaction_date DATE,
     description VARCHAR(200),
     memo TEXT,
     amount DECIMAL(10, 2) NOT NULL,
     transaction_successful BOOLEAN,
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
