26b63a312f3a5a01a0162c36105b390a1dbc6cda
emihhorn <emilyhopehorn@gmail.com>
2021-03-30 09:57:07 -0500

    warm up

---
+++ transactions_migration.sql
@@ -0,0 +1,11 @@
+USE bank_db;
+
+CREATE TABLE IF NOT EXISTS transactions(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    transaction_date new DATE,
+    description VARCHAR(200),
+    memo TEXT,
+    amount DECIMAL(10, 2) NOT NULL,
+    transaction_successful BOOLEAN,
+    PRIMARY KEY (id),
+);
