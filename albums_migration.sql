73f34ea69a2e3597d9f7302d02d2ba6c395023dc
emihhorn <emilyhopehorn@gmail.com>
2021-03-29 20:22:21 -0500

    album

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,13 @@
 USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
-CREATE TABLE IF NOT EXISTS albums(
+
+CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) NOT NULL,
-    name VARCHAR(75) NOT NULL DEFAULT 'Untitled',
-    release_date INT UNSIGNED,
-    sales FLOAT UNSIGNED,
-    genre VARCHAR(50),
+    artist VARCHAR(150) NOT NULL,
+    name VARCHAR(175),
+    release_date SMALLINT UNSIGNED,
+    sales FLOAT (6, 2),
+    genre VARCHAR(150),
     PRIMARY KEY (id)
     );
