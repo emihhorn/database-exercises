da11422802af8bd539971b0888d7794ea30cb891
emihhorn <emilyhopehorn@gmail.com>
2021-03-31 21:08:28 -0500

    order_by

---
+++ order_by_exercises.sql
@@ -0,0 +1,20 @@
+Use employees;
+
+SELECT *
+From employees
+Where (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+  AND gender = 'm';
+
+
+Select *
+From employees
+WHERE last_name Like 'E%'
+  AND last_name Like '%E';
+
+
+
+Select *
+From employees
+WHERE last_name Like '%q%'AND last_name NOT LIKE '%qu%';
