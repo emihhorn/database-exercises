47044cb730b615f9434d6ae0496bef6d8e08e8f0
emihhorn <emilyhopehorn@gmail.com>
2021-04-01 10:43:43 -0500

    order by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,19 +2,38 @@ Use employees;
 
 SELECT *
 From employees
-Where (first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya')
-  AND gender = 'm';
+Where first_name IN ('Irena', 'Vidya')
+    AND last_name IN ('Reutenauer', 'Demeyer')
+    ORDER BY last_name ASC,
+             first_name DESC ;
 
 
-Select *
-From employees
-WHERE last_name Like 'E%'
-  AND last_name Like '%E';
+SELECT *
+FROM employees
+WHERE last_name LIKE '%e%'
+ORDER BY emp_no DESC;
 
-
+SELECT *
+FROM employees
+WHERE first_name IN ('Irene', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
 
 Select *
 From employees
 WHERE last_name Like '%q%'AND last_name NOT LIKE '%qu%';
+
+
+SELECT CONCAT(first_name,' ', last_name)
+FROM employees
+LIMIT 100;
+
+SELECT DAYOFMONTH (birth_date)
+FROM employees
+LIMIT 100;
+
+SELECT NOW();
+
+SELECT UNIX_TIMESTAMP();
+
+#INSERT INTO myTable (name, created_date)
+#VALUES ('Emily', NOW());
