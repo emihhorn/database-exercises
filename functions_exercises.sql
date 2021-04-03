b55faa5fe6c63f91fad8eeeba8719a312b14652c
emihhorn <emilyhopehorn@gmail.com>
2021-04-02 23:26:43 -0500

    functions

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,20 +1,28 @@
-Use employees;
+use employees;
 
 SELECT *
-From employees
-Where (first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya')
-    AND gender = 'm';
+FROM employees
+WHERE first_name IN ('Irena' , 'Vidya', 'Maya')
+  AND gender = 'M'
+ORDER BY last_name DESC, first_name ASC;
+
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
+ORDER BY last_name;
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT CONCAT(first_name, ' ', last_name), DATEDIFF(curdate(), hire_date)
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+  AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY DATEDIFF(curdate(), hire_date);
 
 
-Select *
-From employees
-WHERE last_name Like 'E%'
-  AND last_name Like '%E';
-
-
-
-Select *
-From employees
-WHERE last_name Like '%q%'AND last_name NOT LIKE '%qu%';
+SELECT DISTINCT *
+FROM employees
+WHERE last_name LIKE '%q%'
+ORDER BY last_name;
