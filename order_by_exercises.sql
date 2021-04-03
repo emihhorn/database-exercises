1683ef2ee63e5c1d73e7dbd607b434f5f4aedb69
emihhorn <emilyhopehorn@gmail.com>
2021-04-02 23:27:56 -0500

    order

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -8,7 +8,7 @@ Where first_name IN ('Irena', 'Vidya')
              first_name DESC ;
 
 
-SELECT *
+SELECT * , CONCAT (first_name, ' ', last_name)
 FROM employees
 WHERE last_name LIKE '%e%'
 ORDER BY emp_no DESC;
@@ -21,6 +21,19 @@ ORDER BY last_name, first_name;
 Select *
 From employees
 WHERE last_name Like '%q%'AND last_name NOT LIKE '%qu%';
+
+SELECT *, DATEDIFF(NOW(),hire_date) AS 'Days Employed'
+FROM employees
+WHERE MONTH(birth_date) = 12
+AND DAY(birth_date) = 25
+AND (
+    YEAR (hire_date) BETWEEN 1990 AND 1999
+    )
+ORDER BY birth_date, hire_date DESC;
+
+
+
+
 
 
 SELECT CONCAT(first_name,' ', last_name)
@@ -37,3 +50,5 @@ SELECT UNIX_TIMESTAMP();
 
 #INSERT INTO myTable (name, created_date)
 #VALUES ('Emily', NOW());
+
+
